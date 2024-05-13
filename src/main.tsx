7fbcf5970852de4d2c4035cf51e4de70e0940b11
iArijitGanguly <arijeetGanguly63@gmail.com>
2024-05-14 02:41:54 +0530

    Fix: slight change in routing

--- src/main.tsx
+++ src/main.tsx
@@ -8,9 +8,9 @@ import App from './App.tsx';
 import { store } from './redux/store.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
-    <BrowserRouter>
+  <BrowserRouter>
+    <Provider store={store}>
       <App />
-    </BrowserRouter>
-  </Provider>
+    </Provider>
+  </BrowserRouter>
 );
